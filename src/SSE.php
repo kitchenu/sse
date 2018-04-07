94c9c261db09c9f02611a48b4f977a73998add20
kitchenu <youkitchenu@gmail.com>
2018-04-08 02:49:00 +0900

    Change event loop base

--- src/SSE.php
+++ src/SSE.php
@@ -2,33 +2,46 @@
 
 namespace Kitchenu\Sse;
 
-use Closure;
 use Kitchenu\Sse\Event\EventInterface as Event;
 use Kitchenu\Sse\Event\CallableEvent;
+use GuzzleHttp\Psr7\Response;
+use React\EventLoop\LoopInterface;
+use React\EventLoop\Factory;
+use DateTime;
 
 class SSE
 {
-    protected $id;
+    protected $settings = [
+        'execLimit' => 60,
+        'retryTime' => 1,
+        'keepAliveInterval' => 30,
+        'sendEventName' => true,
+    ];
+
+    /**
+     * @var int
+     */
+    protected $eventId;
+
+    /**
+     * @var Response
+     */
+    protected $response;
+
+    /**
+     * @var LoopInterface
+     */
+    protected $loop;
+
+    /**
+     * @var string
+     */
+    protected $keepMessage = 'keep alive';
+
     /**
      * @var array
      */
-    protected $events = [];
-
-    protected $settings = [
-        'seelpTime'     => 1,
-        'execLimit'     => 600,
-        'reconnectTime' => 1,
-        'keepAliveTime' => 30,
-        'keepMessage'   => 'keep alive',
-    ];
-
-    protected $headers = [
-        'Content-Type'                     => 'text/event-stream',
-        'Cache-Control'                    => 'no-cache',
-        'X-Accel-Buffering'                => 'no',
-        'Access-Control-Allow-Origin'      => '*',
-        'Access-Control-Allow-Credentials' => 'true',
-    ];
+    protected $timers = [];
 
     /**
      * SSE constructor.
@@ -38,8 +51,18 @@ class SSE
      */
     public function __construct($settings = [], $headers = [])
     {
-        $this->headers = array_merge($this->headers, $headers);
-        $this->id = isset($_SERVER['HTTP_LAST_EVENT_ID']) ? (int) $_SERVER['HTTP_LAST_EVENT_ID'] : 0;
+        $this->settings = array_merge($this->settings, $settings);
+        $this->eventId = isset($_SERVER['HTTP_LAST_EVENT_ID']) ? (int) $_SERVER['HTTP_LAST_EVENT_ID'] : 1;
+        $this->response = $this->createResponse($headers);
+        $this->loop = Factory::create();
+    }
+
+    protected function createResponse(array $headers)
+    {
+        return new Response(200, [
+            'Content-Type' => 'text/event-stream',
+            'Cache-Control' => 'no-cache',
+        ] + $headers);
     }
 
     /**
@@ -48,13 +71,43 @@ class SSE
      * @param Event|Closure $event the event handler
      * @return void
      */
-    public function addEvent($name, $event)
+    public function addTimerEvent($name, $event, $interval = 5, $periodic = true)
     {
         if ($event instanceof Event) {
-            $this->events[$name] = $event;
-        } elseif ($event instanceof Closure) {
-            $this->events[$name] = new CallableEvent($name, $event);
+            $this->timers[$name] = $this->loop->addPeriodicTimer($interval, function () use ($name, $event) {
+                if ($event->ready($this->startedTime)) {
+                    $this->sendEvent($event->data(), $event->name());
+                }
+            });
+        } elseif (is_callable($event)) {
+            $this->timers[$name] = $this->loop->addPeriodicTimer($interval, function () use ($name, $event) {
+                $data = call_user_func($event, $this->startedTime);
+                if (!is_null($data)) {
+                    $this->sendEvent($name, $data);
+                }
+            });
         }
+    }
+
+
+    /**
+     * @param string $name
+     * @param string $data
+     * @return string
+     */
+    protected function sendEvent($name, $data)
+    {
+        $message = '';
+
+        if ($this->settings['sendEventName']) {
+            $message .= "\nevent: $name";
+        }
+
+        $message .= "\ndata: $data\nid: {$this->eventId}\n";
+
+        echo $message;
+
+        $this->eventId++;
     }
 
     /**
@@ -63,9 +116,10 @@ class SSE
      * @param string $name the event name
      * @return void
      */
-    public function removeEvent($name)
+    public function removeTimerEvent($name, $event, $interval = 5, $periodic = true)
     {
-        unset($this->events[$name]);
+        $this->loop->cancelTimer($this->timers[$name]);
+        unset($this->timers[$name]);
     }
 
     /**
@@ -76,31 +130,7 @@ class SSE
     public function run()
     {
         $this->init();
-
-        $this->sendHeader();
-
-        echo 'retry: ' . ($this->settings['reconnectTime'] * 1000) . "\n";
-
-        $start = $from = time();
-
-        while (1) {
-            if (time() - $from > $this->settings['keepAliveTime']) {
-                $from = time();
-                // No updates needed, send a comment to keep the connection alive.
-                echo ": {$this->keepMessage}\n\n";
-            }
-
-            foreach ($this->events as $event) {
-                $this->sendEvent($event);
-            }
-
-            // Break if the time exceed the limit
-            if ($this->settings['execLimit'] !== 0 && time() - $start > $this->settings['execLimit'] ) {
-                break;
-            }
-
-            usleep($this->settings['seelpTime']  * 1000000);
-        }
+        $this->loop->run();
     }
 
     /**
@@ -110,20 +140,44 @@ class SSE
      */
     protected function init()
     {
-        @set_time_limit(0); // Disable time limit
-
-        // Prevent buffering
-        if(function_exists('apache_setenv')){
-            @apache_setenv('no-gzip', 1);
+        if ($limit = $this->settings['execLimit']) {
+            $this->loop->addTimer($limit, function () {
+                $this->loop->stop();
+            });
         }
 
-        @ini_set('zlib.output_compression', 0);
-        @ini_set('implicit_flush', 1);
+        $this->loop->futureTick(function () {
+            $this->startedTime = new DateTime();
 
-        while (ob_get_level() != 0) {
-            ob_end_flush();
+            $this->sendHeader();
+
+            if ($this->settings['retryTime']) {
+                $retryTime = $this->settings['retryTime'] * 1000;
+                echo "retry: $retryTime\n";
+            }
+        });
+
+        if ($interval = $this->settings['keepAliveInterval']) {
+            $this->loop->addPeriodicTimer($interval, function () {
+                echo "\n: {$this->keepMessage}\n";
+            }); 
         }
-        ob_implicit_flush(1);
+
+//        @set_time_limit(0); // Disable time limit
+//
+//        // Prevent buffering
+//        if(function_exists('apache_setenv')){
+//            @apache_setenv('no-gzip', 1);
+//        }
+//
+//        @ini_set('zlib.output_compression', 0);
+//        @ini_set('implicit_flush', 1);
+//
+//        while (ob_get_level() != 0) {
+//            ob_end_flush();
+//        }
+//        ob_implicit_flush(1);
+
     }
 
     /**
@@ -131,24 +185,50 @@ class SSE
      */
     protected function sendHeader()
     {
-        foreach ($this->headers as $name => $value) {
-            header($name . ': ' . $value, false);
+        foreach ($this->response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header(sprintf('%s: %s', $name, $value), false);
+            }
+        }
+
+        header(sprintf(
+            'HTTP/%s %s %s',
+            $this->response->getProtocolVersion(),
+            $this->response->getStatusCode(),
+            $this->response->getReasonPhrase()
+        ));
+    }
+
+    public function settings($key = null, $value = null)
+    {
+        if (is_null($key)) {
+            return $this->settings;
+        }
+
+        if (is_array($key)) {
+            $this->settings = array_merge($this->settings, $key);
+        } elseif (is_null($value)) {
+            return $this->settings[$key];
+        } else {
+            $this->settings[$key] = $value;
         }
     }
 
-    /**
-     * @param Event $event
-     * @return void
-     */
-    protected function sendEvent(Event $event)
+    public function loop($loop = null)
     {
-        if ($event->check()) {
-            $this->id++;
-            echo $event->encode($this->id);
-
-            // Make sure the data has been sent to the client
-            @ob_flush();
-            @flush();
+        if (!$loop instanceof LoopInterface) {
+            return $this->loop;
         }
+
+        $this->loop = $loop;
+    }
+
+    public function keepMessage($message = null)
+    {
+        if (is_null($message)) {
+            return $this->keepMessage;
+        }
+
+        $this->keepMessage = (string) $message;
     }
 }
